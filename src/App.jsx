f48001729555f32155a202452de7b545891ef069
Ryceex <ruslanzuabch@gmail.com>
2025-05-14 21:37:15 +0300

    added calculator page

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,19 @@ import Workouts from "./pages/Workouts";
 import Header from "./components/general-components/Header";
 import Footer from "./components/general-components/Footer";
 import ContactSection from "./components/general-components/ContactSection";
+import Calculator from "./pages/Calculator";
 
 function App() {
   return (
     <Router>
       <>
-      <Header />
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/recipes" element={<Recipes />} />
           <Route path="/recipe" element={<RecipePage />} />
           <Route path="/workouts" element={<Workouts />} />
+          <Route path="/calories" element={<Calculator />} />
         </Routes>
         <ContactSection />
         <Footer />
